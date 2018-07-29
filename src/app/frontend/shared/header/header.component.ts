b265474a87ab17770462161d9b04c8841ed6ccda
Ash Negi <ashish77negi@gmail.com>
2018-07-29 15:02:38 +0530

    Search fixes

--- src/app/frontend/shared/header/header.component.ts
+++ src/app/frontend/shared/header/header.component.ts
@@ -54,7 +54,7 @@ export class HeaderComponent implements OnInit {
 
   onChanges(): void {
     this.searchForm.get('search').valueChanges.subscribe(val => {
-      if (val !== null) {
+      if (val !== null && val !== '') {
         const searchText = val.toLowerCase();
         const x = this.prodService.getProductsList();
         x.snapshotChanges().subscribe(item => {
